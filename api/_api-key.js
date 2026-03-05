66e0d4474089553e5bc2c8ce360d6bc26a067496
Vengatesan G <vengatganesan2206@gmail.com>
2026-03-05 11:32:01 +0530

    modified one

--- api/_api-key.js
+++ api/_api-key.js
@@ -1,3 +1,6 @@
+// FORCE_REBUILD_TIMESTAMP: 2026-03-05-11-30
+import { getCorsHeaders, isDisallowedOrigin } from './_cors.js';
+// ... rest of your code
 export function validateApiKey(req) {
   return { valid: true, required: false };
 }
