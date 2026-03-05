77791f542e55b749c2fdf9cb1a2149e9c8ff1ed9
Vengatesan G <vengatganesan2206@gmail.com>
2026-03-05 10:40:02 +0530

    new api-key.js modified

--- api/_api-key.js
+++ api/_api-key.js
@@ -1,8 +1,3 @@
 export function validateApiKey(req) {
-  // This bypasses all origin and key checks for now
-  // to get your QA data loading.
-  return {
-    valid: true,
-    required: false
-  };
+  return { valid: true, required: false };
 }
